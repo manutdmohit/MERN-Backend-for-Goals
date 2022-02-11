85dff1371d7dad0b080e9236b07edb83f4807f9d
manutdmohit <saudmohit@gmail.com>
2022-02-11 21:41:50 +0545

    installing express-async-handler

--- backend/goalControllers/goalController.js
+++ backend/goalControllers/goalController.js
@@ -1,37 +1,44 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private
-exports.getGoals = (req, res) => {
+exports.getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'Goals',
   });
-};
+});
 
 // @desc Set Goal
 // @route POST /api/goals
 // @access Private
 
-exports.setGoal = (req, res) => {
+exports.setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+
+    throw new Error('Please add a text field');
+  }
   res.status(200).json({
     message: 'Set Goals',
   });
-};
+});
 
 // @desc  Update Goal
 // @route PUT /api/goals/:id
 // @access Private
 
-exports.updateGoal = (req, res) => {
+exports.updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Update goal ${req.params.id}`,
   });
-};
+});
 
 // @desc Delete Goal
 // @route DELETE /api/goals/:id
 // @access Private
-exports.deleteGoal = (req, res) => {
+exports.deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Deleted goal ${req.params.id}`,
   });
-};
+});
