b9cabacf2ee33c3caab9309345b94912a28d4cb9
manutdmohit <saudmohit@gmail.com>
2022-02-11 10:28:01 +0545

    adding first route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('Hello Goals');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+  res.status(200).json({
+    message: 'Goals',
+  });
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
