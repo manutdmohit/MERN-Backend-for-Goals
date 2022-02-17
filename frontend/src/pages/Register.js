147ed0b14127903276ec72145911824a01ae2168
manutdmohit <saudmohit@gmail.com>
2022-02-17 12:01:35 +0545

    Register Page

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,90 @@
+import { useState, useEffect } from 'react';
+import { FaUser } from 'react-icons/fa';
+
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    cpassword: '',
+  });
+
+  const { name, email, password, cpassword } = formData;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Create An Account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className='="form-control'
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Enter Your Name"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className='="form-control'
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter Your Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className='="form-control'
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter Your Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className='="form-control'
+              id="cpassword"
+              name="cpassword"
+              value={cpassword}
+              placeholder="Confirm Password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Register;
