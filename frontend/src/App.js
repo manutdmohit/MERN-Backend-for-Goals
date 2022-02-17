48ff0337670350190d6f592c9e28e5980d3c76b8
manutdmohit <saudmohit@gmail.com>
2022-02-17 10:14:37 +0545

    React Router Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      My App
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
