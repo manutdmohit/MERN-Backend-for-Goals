093b2fbd4b58b955b1199441cfc1821f3115cef0
manutdmohit <saudmohit@gmail.com>
2022-02-18 10:35:09 +0545

    Fix user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,13 +77,7 @@ exports.loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 // @access Private
 exports.getMe = asyncHandler(async (req, res) => {
-  const { _id, name, email } = await User.findById(req.user.id);
-
-  res.status(200).json({
-    id: _id,
-    name,
-    email,
-  });
+  res.status(200).json(req.user);
 });
 
 // Generate JWT
